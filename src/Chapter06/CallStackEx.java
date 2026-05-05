7442dea7ceb4035cd6ae36e76ccfa68b5f4b144f
rayeongu <rayeongu@gmail.com>
2026-05-05 22:26:00 +0900

    feat: ch06 ex6-7

---
+++ src/Chapter06/CallStackEx.java
@@ -0,0 +1,15 @@
+package Chapter06;
+
+public class CallStackEx {
+    public static void main(String[] args) {
+        firstMethod();
+    }
+
+    static void firstMethod() {
+        secondMethod();
+    }
+
+    static void secondMethod() {
+        System.out.println("secondMethod()");
+    }
+}
