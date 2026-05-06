68328064c52fd11c84983d8747bafc78698d40c3
rayeongu <rayeongu@gmail.com>
2026-05-06 23:09:10 +0900

    feat: ch06 ex6-9

--- src/Chapter06/PrimitiveParamEx.java
+++ src/Chapter06/PrimitiveParamEx.java
@@ -1,6 +1,8 @@
 package Chapter06;
 
-class Data {int x;}
+class Data {
+    int x;
+}
 
 public class PrimitiveParamEx {
     public static void main(String[] args) {
@@ -13,7 +15,7 @@ public class PrimitiveParamEx {
         System.out.println("main() : x = " + d.x);
     }
 
-    static void change(int x){
+    static void change(int x) {
         x = 1000;
         System.out.println("change() : x = " + x);
     }
