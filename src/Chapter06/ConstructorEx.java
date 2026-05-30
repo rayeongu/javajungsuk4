b970ff2d2341e70976a48a784ad8bb627a11e7e7
rayeongu <rayeongu@gmail.com>
2026-05-31 02:07:21 +0900

    fix: ch06 ex6-23

--- src/Chapter06/ConstructorEx.java
+++ src/Chapter06/ConstructorEx.java
@@ -15,6 +15,6 @@ class Data_2 {
 public class ConstructorEx {
     public static void main(String[] args) {
         Data_1 d1 = new Data_1();
-        Data_2 d2 = new Data_2(); // compile error 발생
+        //Data_2 d2 = new Data_2(); // compile error 발생
     }
 }
