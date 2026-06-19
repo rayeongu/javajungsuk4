448b172ad4f66b052dbbba9d43381a491c6eacae
rayeongu <rayeongu@gmail.com>
2026-06-19 22:20:34 +0900

    feat: ch07 ex7-11

---
+++ src/Chapter07/StaticImportEx.java
@@ -0,0 +1,15 @@
+package Chapter07;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.random;
+import static java.lang.System.out;
+
+public class StaticImportEx {
+    public static void main(String[] args) {
+        // System.out.println(Math.random());
+        out.println(random());
+
+        // System.out.println("Math.PI :"+Math.PI);
+        out.println("Math.PI :" + PI);
+    }
+}
