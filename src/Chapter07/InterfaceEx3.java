24b08e580d4d71de0f2806a763f1002b46ff02b7
rayeongu <rayeongu@gmail.com>
2026-07-09 08:49:43 +0900

    feat: ch07 ex7-31

---
+++ src/Chapter07/InterfaceEx3.java
@@ -0,0 +1,36 @@
+package Chapter07;
+
+class InterfaceEx3 {
+    public static void main(String[] args) {
+        A3 a = new A3();
+        a.methodA();
+    }
+}
+
+class A3 {
+    void methodA() {
+        I3 i = InstanceManager.getInstance();
+        i.methodB();
+        System.out.println(i.toString());
+    }
+}
+
+interface I3 {
+    public abstract void methodB();
+}
+
+class B3 implements I3 {
+    public void methodB() {
+        System.out.println("methodB in B class");
+    }
+
+    public String toString() {
+        return "class B";
+    }
+}
+
+class InstanceManager {
+    public static I3 getInstance() {
+        return new B3();
+    }
+}
